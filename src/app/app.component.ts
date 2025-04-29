b64e03ce2083c4d3ccd46cee9b7ad6117327e0d9
AbdelNZ <afenzar.a@gmail.com>
2025-04-29 01:28:32 +0100

    starting project

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { PlayingCardComponent } from './components/playing-card/playing-card.component';
+import { Component } from '@angular/core';
+
+
+@Component({
+	selector: 'app-root',
+	standalone: true,
+  imports :[PlayingCardComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'playing-cards';
+}
